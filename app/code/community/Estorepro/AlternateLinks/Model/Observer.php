440fe5e4bf90175231871c0a7f8505ed69d4ae26
Ben Leah <benleahdesign@gmail.com>
2014-11-11 21:44:00 +0000

    Adds comments to Observer.php

--- app/code/community/Estorepro/AlternateLinks/Model/Observer.php
+++ app/code/community/Estorepro/AlternateLinks/Model/Observer.php
@@ -34,26 +34,44 @@ class Estorepro_AlternateLinks_Model_Observer
 
     public function alternateLinks()
     {
+        //Stores Magento head structural block as a variable 
         $headBlock = Mage::app()->getLayout()->getBlock('head');
+        
+        //Gets all stores for the website
         $stores = Mage::app()->getWebsite()->getStores();
+        
+        //Gets current page URL
         $currentUrl = Mage::helper('core/url')->getCurrentUrl();
         
+        //Test to see if the module is disabled
         if(Mage::getStoreConfig("web/alternatelinks/enable_module") != false)
         {
+            //Test to make sure the native head structural block is exists
             if($headBlock)
             {
                 foreach ($stores as $store) 
                 {
+                    //gets the store language and region in native xx_XX format
                     $lang = $store->getConfig('general/locale/code');
+                    
+                    //Strips parameters from the core URL which are not present in the 'actual' URL and stores in a new variable
                     $cleanUrl = preg_replace('/\?.*/', '', $store->getCurrentUrl());
                     
+                    //Tests config setting in admin for the hreflang format
                     if(Mage::getStoreConfig("web/alternatelinks/hreflang_value") == 'language') {
+                        
+                        // Strips locale string down to th first 2 characters - matching the ISO 639-1 format
                         $lang = substr($lang, 0, 2);
                     } elseif(Mage::getStoreConfig("web/alternatelinks/hreflang_value") == 'language-region') {
+                        
+                        // Replaces the native Magento '_' with a '-' to match the ISO 3166-1 Alpha 2 format
                         $lang = preg_replace("/[\s_]/", "-", $lang);
                     }
-            
+                    
+                    //Test to see if store in the array is not the current store (to show alternates only - not 'this' store)
                     if($cleanUrl != $currentUrl) {
+                        
+                        //Output the rel link using the native addLinkRel() method
                         $headBlock->addLinkRel('alternate"' . ' hreflang="' . $lang, $cleanUrl);
                     }
                 }
